b7f3000091333e44961bd733a3719c3f1c2fd466
MD EJAZ ALAM <108115025+ejaz-cedcoss@users.noreply.github.com>
2022-08-08 23:01:02 +0530

    responsive navbar

---
+++ navbar.js
@@ -0,0 +1,14 @@
+const hideshow= ()=>{
+  
+  
+
+
+    var x = document.getElementById("sidebarMenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  
+
+}
